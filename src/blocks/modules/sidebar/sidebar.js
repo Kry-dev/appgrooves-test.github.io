e13afc4a6a1dd7803411036a14e351b1238b4a12
kry-dev <yaroslavkryvda@gmail.com>
2019-07-13 20:58:10 +0300

    first commit

---
+++ src/blocks/modules/sidebar/sidebar.js
@@ -0,0 +1,13 @@
+modules.define('sidenav', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
