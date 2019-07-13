a7636d4e4ffb290550b51011718000ee20bb9d48
kry-dev <yaroslavkryvda@gmail.com>
2019-07-14 00:45:07 +0300

    add sliders

---
+++ src/blocks/modules/best-apps/best-apps.js
@@ -0,0 +1,13 @@
+modules.define('best-apps', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
